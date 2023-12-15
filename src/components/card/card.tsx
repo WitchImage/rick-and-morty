53c6658d75f2941635bba6733c4e136697ecb628
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-15 16:56:19 -0500

    feat: add card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@/lib/utils/cn';
+import { type HTMLAttributes } from 'react';
+
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export default function Card({ children, className }: CardProps) {
+    return (
+        <div
+            className={cn(
+                'rounded-md bg-white flex flex-col p-6 items-center shadow-md border-gray-500',
+                className
+            )}
+        >
+            {children}
+        </div>
+    );
+}
