6e20d9ed5183cebc72bf09e9fc8a7f02e8294002
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-15 17:40:53 -0500

    feat: add Apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/home/home.tsx';
 import ROUTES from './lib/constants/routes.ts';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+console.log(import.meta.env.REACT_APP_PUBLIC_API);
+
+const client = new ApolloClient({
+    uri: 'https://rickandmortyapi.com/graphql',
+    cache: new InMemoryCache(),
+});
 
 const router = createBrowserRouter([
     {
@@ -14,7 +22,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ApolloProvider client={client}>
+            <RouterProvider router={router} />
+        </ApolloProvider>
     </React.StrictMode>
 );
 
