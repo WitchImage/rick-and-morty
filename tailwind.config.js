f586c82c76b36a23d047efbdb9732eda7292721b
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-15 16:54:28 -0500

    feat: add transform transition property

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,9 @@ export default {
             borderRadius: {
                 md: 'var(--border-radius-md)',
             },
+            transitionProperty: {
+                transform: 'transform',
+            },
         },
     },
     plugins: [],
