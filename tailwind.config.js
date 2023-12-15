47d5fa94677bd9f03c391e0c574e545cc1ce5a3b
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-15 14:14:03 -0500

    build: add tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    100: 'var(--primary-color-100)',
+                    600: 'var(--primary-color-600)',
+                    700: 'var(--primary-color-700)',
+                },
+                secondary: {
+                    600: 'var(--secondary-color-600)',
+                },
+            },
+        },
+    },
+    plugins: [],
+};
+
