03cbe509a9be3d7d479d9de17f1ed6c319bf1c08
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-15 23:37:18 -0500

    feat: add navigation to character page

--- src/pages/home/components/character-card.tsx
+++ src/pages/home/components/character-card.tsx
@@ -1,24 +1,30 @@
 import Card from '@/components/card/card.tsx';
+import ROUTES from '@/lib/constants/routes';
 import { type Character } from '@/types';
+import { useNavigate } from 'react-router-dom';
 
 interface CharacterCardProps {
     character: Character;
 }
 
 export default function CharacterCard({ character }: CharacterCardProps) {
-    const { name, image, species } = character;
+    const { id, name, image, species } = character;
+    const navigate = useNavigate();
 
     return (
-        <Card className="relative h-[300px] transition-transform hover:scale-[1.1] hover:cursor-pointer">
-            <span className="z-10 text-2xl text-white font-bold text-center">
+        <Card
+            className="relative h-[300px] transition-transform hover:scale-[1.1] hover:cursor-pointer"
+            onClick={() => navigate(`${ROUTES.CHARACTERS}/${id}`)}
+        >
+            <span className="z-10 text-2xl font-bold text-center text-white">
                 {name}
             </span>
             <img
                 src={image}
                 alt={`Imagen de ${name}`}
-                className="rounded-md absolute h-full w-full"
+                className="absolute w-full h-full rounded-md"
             />
-            <span className="mb-4 z-10 text-white text-sm font-medium absolute bottom-0 bg-primary-600 py-1 px-2 rounded-full">
+            <span className="absolute bottom-0 z-10 px-2 py-1 mb-4 text-sm font-medium text-white rounded-full bg-primary-600">
                 {species}
             </span>
         </Card>
