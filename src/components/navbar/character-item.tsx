d0791935a79af25899dbf6b87993bd327f6c663a
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-16 09:32:39 -0500

    refactor: add star-button component

--- src/components/navbar/character-item.tsx
+++ src/components/navbar/character-item.tsx
@@ -1,34 +1,19 @@
 import ROUTES from '@/lib/constants/routes';
-import useCharacterStore from '@/store/character-store';
 import { type Character } from '@/types';
-import { IoIosHeart, IoMdHeartEmpty } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
+import StarButton from '../star-button';
 
 interface CharacterItemProps {
     character: Character;
     isStarred: boolean;
 }
 
-export default function CharacterItem({
-    character,
-    isStarred,
-}: CharacterItemProps) {
+export default function CharacterItem({ character }: CharacterItemProps) {
     const { id, name, image, species } = character;
-    const { addStarredCharacter, removeStarredCharacter } = useCharacterStore();
     const navigate = useNavigate();
 
     const handleItemClick = () => {
         navigate(`${ROUTES.CHARACTERS}/${id}`);
-    };
-
-    const handleUnstarCharacterIconClick = (e: React.MouseEvent) => {
-        e.stopPropagation();
-        removeStarredCharacter(character.id);
-    };
-
-    const handleStarCharacterIconClick = (e: React.MouseEvent) => {
-        e.stopPropagation();
-        addStarredCharacter(character);
     };
 
     return (
@@ -47,19 +32,10 @@ export default function CharacterItem({
                 <span className="block font-bold">{name}</span>
                 <span className="text-sm">{species}</span>
             </div>
-            {isStarred ? (
-                <IoIosHeart
-                    size={25}
-                    className="z-10 absolute right-0 mr-[20px] hover:cursor-pointer fill-secondary-600 hover:fill-red-400"
-                    onClick={handleUnstarCharacterIconClick}
-                />
-            ) : (
-                <IoMdHeartEmpty
-                    size={25}
-                    className="z-10 absolute right-0 mr-[20px] hover:cursor-pointer hover:fill-secondary-600"
-                    onClick={handleStarCharacterIconClick}
-                />
-            )}
+            <StarButton
+                character={character}
+                className="z-10 absolute right-0 mr-[20px]"
+            />
         </li>
     );
 }
