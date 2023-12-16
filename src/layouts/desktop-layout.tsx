57da976920f96c3e46ba188e004ee9743cf78fb7
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-16 14:22:39 -0500

    refactor: rename desktop navbar

--- src/layouts/desktop-layout.tsx
+++ src/layouts/desktop-layout.tsx
@@ -1,10 +1,10 @@
 import { PropsWithChildren } from 'react';
-import DesktopNavbar from '../components/navbar/desktop-navbar';
+import CharactersNavbar from '../components/navbar/characters-navbar';
 
 export default function DesktopLayout({ children }: PropsWithChildren) {
     return (
         <div className="grid grid-cols-[375px_1fr] min-h-screen">
-            <DesktopNavbar />
+            <CharactersNavbar />
             <div>{children}</div>
         </div>
     );
