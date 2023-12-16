f85f16c4c0c16765cc4329acb67f51ed49f37d69
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-16 14:55:23 -0500

    refactor: add characters navbar to the layout

--- src/layouts/mobile-layout.tsx
+++ src/layouts/mobile-layout.tsx
@@ -1,11 +1,22 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import MobileNavbar from '@/components/navbar/mobile-navbar';
+import CharactersNavbar from '@/components/navbar/characters-navbar';
 
 export default function MobileLayout({ children }: PropsWithChildren) {
+    const [showCharactersNavar, setShowCharactersNavbar] =
+        useState<boolean>(false);
+
     return (
         <div className="min-h-screen">
-            <div className="pb-[83px]">{children}</div>
-            <MobileNavbar />
+            {showCharactersNavar ? (
+                <CharactersNavbar
+                    setShow={setShowCharactersNavbar}
+                    className="w-full min-h-screen bg-white peer"
+                />
+            ) : (
+                <div className="pb-[83px]">{children}</div>
+            )}
+            <MobileNavbar setShowCharactersNavbar={setShowCharactersNavbar} />
         </div>
     );
 }
