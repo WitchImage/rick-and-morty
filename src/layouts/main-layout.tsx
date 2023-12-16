0ce9d0a74ba026c530f267c372f744dc2a33beae
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-16 11:20:14 -0500

    feat: add layout change on windows dimension change

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,10 +1,15 @@
 import { PropsWithChildren } from 'react';
 import DesktopLayout from './desktop-layout';
+import useWindowDimensions from '@/hooks/use-window-dimension';
+import MobileLayout from './mobile-layout';
+
+const MOBILE_BREAKPOINT = 1024;
 
 export default function MainLayout({ children }: PropsWithChildren) {
-    return (
-        <>
-            <DesktopLayout>{children}</DesktopLayout>
-        </>
-    );
+    const { width } = useWindowDimensions();
+
+    if (width > MOBILE_BREAKPOINT)
+        return <DesktopLayout>{children}</DesktopLayout>;
+
+    return <MobileLayout>{children}</MobileLayout>;
 }
