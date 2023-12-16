afd49ee0167263f7906fe57bc5a1cf3a5c864ef9
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-16 18:03:50 -0500

    chore: add types for species and filters

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,14 @@
+export type Species = 'Human' | 'Alien';
+
 export type Character = {
     id: string;
     name: string;
     status: string;
-    species: string;
+    species: Species;
     type: string;
     gender: string;
     image: string;
 };
+
+export type Characterfilter = 'Starred' | 'Others' | 'All';
+export type SpeciesFilter = Species | 'All';
