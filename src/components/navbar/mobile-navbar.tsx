adfe0a3d26730a1d991427eb34c2345fdc9b7ea8
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-16 14:54:59 -0500

    feat: add show characters navbar action

--- src/components/navbar/mobile-navbar.tsx
+++ src/components/navbar/mobile-navbar.tsx
@@ -4,7 +4,13 @@ import { HiOutlineSquare2Stack } from 'react-icons/hi2';
 import Button from '../button/button';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
-export default function MobileNavbar() {
+interface MobileNavbarProps {
+    setShowCharactersNavbar: (value: boolean) => void;
+}
+
+export default function MobileNavbar({
+    setShowCharactersNavbar,
+}: MobileNavbarProps) {
     return (
         <nav className="z-10 fixed w-full bottom-0 h-[63px] bg-gray-500 flex flex-row justify-between p-3 shadow-lg">
             <Button variant="unstyled">
@@ -17,7 +23,11 @@ export default function MobileNavbar() {
                 <BsBoxArrowUp size={25} className="fill-mblue" />
             </Button>
             <Button variant="unstyled">
-                <IoBookOutline size={25} className="stroke-mblue" />
+                <IoBookOutline
+                    size={25}
+                    className="stroke-mblue"
+                    onClick={() => setShowCharactersNavbar(true)}
+                />
             </Button>
             <Button variant="unstyled">
                 <HiOutlineSquare2Stack size={25} className="stroke-mblue" />
