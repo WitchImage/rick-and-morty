b8a1358a46730861a7d4058ba89cffa2101aba17
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-16 11:19:37 -0500

    feat: add mobile navbar component

---
+++ src/components/navbar/mobile-navbar.tsx
@@ -0,0 +1,7 @@
+export default function MobileNavbar() {
+    return (
+        <nav className="z-10 fixed w-full bottom-0 h-[83px] bg-gray-500">
+            <span>Mobile navbar</span>
+        </nav>
+    );
+}
