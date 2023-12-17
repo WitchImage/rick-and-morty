da7e2683dba4610155000a3b52ee383de691bbdd
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-17 10:46:33 -0500

    feat: add persistent characters comments to store

--- src/store/character-store.ts
+++ src/store/character-store.ts
@@ -1,34 +1,71 @@
-import { type Character } from '@/types';
+import type { CharacterComments, Character, Comment } from '@/types';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface CommentToPost {
+    characterId: string;
+    message: string;
+}
 
 interface CharacterStore {
     characters: Character[];
     starredCharacters: { [key: string]: Character };
+    characterComments: CharacterComments;
     setCharacters: (characters: Character[]) => void;
     addStarredCharacter: (character: Character) => void;
     removeStarredCharacter: (characterId: string) => void;
+    addComent: (comment: CommentToPost) => void;
 }
 
-const useCharacterStore = create<CharacterStore>()((set) => ({
-    characters: [],
-    starredCharacters: {},
-    setCharacters: (c) => set(() => ({ characters: c })),
-    addStarredCharacter: (c) =>
-        set((state) => {
-            const sc = { ...state.starredCharacters };
-            sc[c.id] = c;
-            return {
-                starredCharacters: sc,
-            };
+const useCharacterStore = create<CharacterStore>()(
+    persist(
+        (set) => ({
+            characters: [],
+            starredCharacters: {},
+            characterComments: {},
+            setCharacters: (c) => set(() => ({ characters: c })),
+            addStarredCharacter: (c) =>
+                set((state) => {
+                    const sc = { ...state.starredCharacters };
+                    sc[c.id] = c;
+                    return {
+                        starredCharacters: sc,
+                    };
+                }),
+            removeStarredCharacter: (characterId) =>
+                set((state) => {
+                    const sc = { ...state.starredCharacters };
+                    delete sc[characterId];
+                    return {
+                        starredCharacters: sc,
+                    };
+                }),
+            addComent: (comment) =>
+                set((state) => {
+                    const { message, characterId } = comment;
+                    const comments = { ...state.characterComments };
+
+                    const newComment: Comment = {
+                        message: message,
+                        createdAt: new Date().toISOString(),
+                    };
+
+                    if (!comments[characterId])
+                        comments[characterId] = [newComment];
+                    else comments[characterId].unshift(newComment);
+
+                    return {
+                        characterComments: comments,
+                    };
+                }),
         }),
-    removeStarredCharacter: (characterId) =>
-        set((state) => {
-            const sc = { ...state.starredCharacters };
-            delete sc[characterId];
-            return {
-                starredCharacters: sc,
-            };
-        }),
-}));
+        {
+            name: 'comments',
+            partialize: (state) => ({
+                characterComments: state.characterComments,
+            }),
+        }
+    )
+);
 
 export default useCharacterStore;
