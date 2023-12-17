4403d36fa69f0008747980e40b831081243ed34f
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-17 10:45:34 -0500

    feat: add characterId prop and comments list

--- src/pages/character/components/comments.tsx
+++ src/pages/character/components/comments.tsx
@@ -1,13 +1,24 @@
 import Button from '@/components/button/button';
+import useCharacterStore from '@/store/character-store';
 import { useState } from 'react';
+import CommentsList from './comments-list';
 
-export default function Comments() {
+interface CommentsProps {
+    characterId: string;
+}
+
+export default function Comments({ characterId }: CommentsProps) {
+    const { addComent, characterComments } = useCharacterStore();
     const [comment, setComment] = useState<string>('');
+    const comments = characterComments[characterId];
 
     return (
         <section className="w-full">
             <h2 className="mb-3 text-xl font-bold">Comentarios</h2>
-            <div className="w-full">
+            <form
+                className="w-full"
+                onSubmit={() => addComent({ characterId, message: comment })}
+            >
                 <label htmlFor="comment" className="block mb-2 font-semibold">
                     Write a new comment
                 </label>
@@ -18,11 +29,15 @@ export default function Comments() {
                     onChange={(e) => setComment(e.target.value)}
                     value={comment}
                 />
-                <Button variant="gray" className="w-full max-w-[350px] my-5">
-                    Save
+                <Button
+                    variant="gray"
+                    className="w-full max-w-[350px] my-5"
+                    type="submit"
+                >
+                    Post
                 </Button>
-            </div>
-            <div></div>
+            </form>
+            <CommentsList comments={comments} />
         </section>
     );
 }
