4de85fc0617ab0d930dcba0ae58cfc5ca1e64824
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-17 16:40:29 -0500

    feat: add orderBy actions to store

--- src/store/character-store.ts
+++ src/store/character-store.ts
@@ -2,15 +2,19 @@ import type { CharacterComments, Character, Comment } from '@/types';
 import { create } from 'zustand';
 import { persist, devtools } from 'zustand/middleware';
 
+type OrderBy = 'none' | 'A-Z' | 'Z-A';
+
 interface CommentToPost {
     characterId: string;
     message: string;
 }
 
 interface CharacterStore {
+    orderBy: OrderBy;
     characters: Character[];
     starredCharacters: { [key: string]: Character };
     characterComments: CharacterComments;
+    setOrderBy: (value: OrderBy) => void;
     setCharacters: (characters: Character[]) => void;
     addStarredCharacter: (character: Character) => void;
     removeStarredCharacter: (characterId: string) => void;
@@ -21,9 +25,27 @@ const useCharacterStore = create<CharacterStore>()(
     devtools(
         persist(
             (set) => ({
+                orderBy: 'none',
                 characters: [],
                 starredCharacters: {},
                 characterComments: {},
+                setOrderBy: (value) =>
+                    set((state) => {
+                        const c = [...state.characters];
+                        if (value === 'A-Z')
+                            c.sort((a, b) =>
+                                a.name < b.name ? -1 : a.name > b.name ? 1 : 0
+                            );
+                        else if (value === 'Z-A')
+                            c.sort((a, b) =>
+                                a.name < b.name ? 1 : a.name > b.name ? -1 : 0
+                            );
+
+                        return {
+                            orderBy: value,
+                            characters: c,
+                        };
+                    }),
                 setCharacters: (c) => set(() => ({ characters: c })),
                 addStarredCharacter: (c) =>
                     set((state) => {
