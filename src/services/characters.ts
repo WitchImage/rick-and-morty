c04f3bd98016410b715c7ae65f07c679675323cb
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-16 23:31:04 -0500

    feat: filters to getCharacters query

--- src/services/characters.ts
+++ src/services/characters.ts
@@ -1,13 +1,33 @@
+import type { Characterfilter, SpeciesFilter } from '@/types';
 import { gql } from '@apollo/client';
 
 interface getCharactersParams {
     page: number;
+    filters?: {
+        characterFilter: Characterfilter;
+        speciesFilter: SpeciesFilter;
+    };
 }
 
-export function getCharacters({ page }: getCharactersParams) {
+export function getCharacters({
+    page,
+    filters = { characterFilter: 'All', speciesFilter: 'All' },
+}: getCharactersParams) {
+    const getFilters = () => {
+        let queryFilters = '{';
+
+        const { speciesFilter } = filters;
+        if (speciesFilter !== 'All')
+            queryFilters += `species: "${speciesFilter}"`;
+
+        queryFilters += '}';
+        return queryFilters;
+    };
+    getFilters();
+
     return gql`
         query Characters {
-            characters(page: ${page}) {
+            characters(page: ${page}, filter: ${getFilters()}) {
                 results {
                     id
                     name
