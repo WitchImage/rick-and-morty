23af063822519bb9e2d83976585412e63fe91222
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-17 19:54:14 -0500

    feat: add use-debounce hook

---
+++ src/hooks/use-debounce.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const useDebounce = (value: any, delay: number) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [value, delay]);
+
+    return debouncedValue;
+};
+
+export default useDebounce;
