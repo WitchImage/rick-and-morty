7d59ca7f640dfcab692c409abcda0b628f2f1c09
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-17 19:55:31 -0500

    feat: add gender and status filters to store

--- src/store/filters-store.ts
+++ src/store/filters-store.ts
@@ -1,12 +1,23 @@
-import type { Characterfilter, SpeciesFilter } from '@/types';
+import type {
+    Characterfilter,
+    Filters,
+    GenderFilter,
+    SpeciesFilter,
+    StatusFilter,
+} from '@/types';
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
 interface FiltersStore {
     characterFilter: Characterfilter;
     speciesFilter: SpeciesFilter;
+    statusFilter: StatusFilter;
+    genderFilter: GenderFilter;
     setCharacterFilter: (value: Characterfilter) => void;
     setSpeciesFilter: (value: SpeciesFilter) => void;
+    setStatusFilter: (value: StatusFilter) => void;
+    setGenderFilter: (value: GenderFilter) => void;
+    setFilters: (filters: Filters) => void;
 }
 
 const useFiltersStore = create<FiltersStore>()(
@@ -14,6 +25,8 @@ const useFiltersStore = create<FiltersStore>()(
         (set) => ({
             characterFilter: 'All',
             speciesFilter: 'All',
+            statusFilter: 'All',
+            genderFilter: 'All',
             setCharacterFilter: (value) =>
                 set(() => ({
                     characterFilter: value,
@@ -22,6 +35,29 @@ const useFiltersStore = create<FiltersStore>()(
                 set(() => ({
                     speciesFilter: value,
                 })),
+            setStatusFilter: (value) =>
+                set(() => ({
+                    statusFilter: value,
+                })),
+            setGenderFilter: (value) =>
+                set(() => ({
+                    genderFilter: value,
+                })),
+            setFilters: (filters) =>
+                set(() => {
+                    const {
+                        characterFilter,
+                        speciesFilter,
+                        genderFilter,
+                        statusFilter,
+                    } = filters;
+                    return {
+                        characterFilter,
+                        speciesFilter,
+                        genderFilter,
+                        statusFilter,
+                    };
+                }),
         }),
         { name: 'filterStore' }
     )
