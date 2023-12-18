a5a98412dd700b9a8ad523c1fb7a1340ad6f57be
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-18 18:08:46 -0500

    fix: set total results correctly

--- src/components/navbar/filterred-characters-info.tsx
+++ src/components/navbar/filterred-characters-info.tsx
@@ -2,15 +2,18 @@ import useCharacterStore from '@/store/character-store';
 import useFiltersStore from '@/store/filters-store';
 
 export default function FilterredCharactersInfo() {
-    const { characters } = useCharacterStore();
+    const { characters, starredCharacters } = useCharacterStore();
     const { characterFilter, speciesFilter, genderFilter, statusFilter } =
         useFiltersStore();
 
     const totalCharacters = characters.length;
 
-    // const totalResults = () => {
+    const totalResults = () => {
+        if (characterFilter === 'Others')
+            return totalCharacters - Object.values(starredCharacters).length;
 
-    // };
+        return totalCharacters;
+    };
 
     const totalFilters =
         (characterFilter !== 'All' ? 1 : 0) +
@@ -22,7 +25,7 @@ export default function FilterredCharactersInfo() {
 
     return (
         <section className="w-full pl-[58px] pr-[24px] flex flex-row justify-between">
-            <span className="font-semibold text-mblue">{`${totalCharacters} Result${
+            <span className="font-semibold text-mblue">{`${totalResults()} Result${
                 totalCharacters > 1 ? 's' : ''
             }`}</span>
             <div className="px-[12px] py-[2px] bg-secondary-100 text-secondary-600 font-semibold rounded-full">
