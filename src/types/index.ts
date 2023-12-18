ddb22c205ae4795d4a252cb2f06798ff27afbd27
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-17 19:54:57 -0500

    chore: add types for gender and status and their respective filters

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,27 @@
 export type Species = 'Human' | 'Alien';
+export type Status = 'Alive' | 'Dead' | 'Unknown';
+export type Gender = 'Female' | 'Male' | 'Genderless' | 'Unknown';
 
 export type Character = {
     id: string;
     name: string;
-    status: string;
+    status: Status;
     species: Species;
     type: string;
-    gender: string;
+    gender: Gender;
     image: string;
 };
 
 export type Characterfilter = 'Starred' | 'Others' | 'All';
 export type SpeciesFilter = Species | 'All';
+export type StatusFilter = Status | 'All';
+export type GenderFilter = Gender | 'All';
+export type Filters = {
+    characterFilter: Characterfilter;
+    speciesFilter: SpeciesFilter;
+    statusFilter: StatusFilter;
+    genderFilter: GenderFilter;
+};
 
 export type Comment = {
     message: string;
