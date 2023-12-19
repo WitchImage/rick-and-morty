1b6d5b8510590e17b7e94ead9f095c6bb7a7760b
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-12-18 23:38:40 -0500

    feat: add error boundary to routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { Suspense, lazy } from 'react';
 import ROUTES from './lib/constants/routes';
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorBoundary from './components/error-boundary';
 
 const HomePage = lazy(() => import('@/pages/home/home.tsx'));
 const CharacterPage = lazy(() => import('@/pages/character/character.tsx'));
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
                 <HomePage />
             </Suspense>
         ),
+        errorElement: <ErrorBoundary />,
     },
     {
         path: `${ROUTES.CHARACTERS}/:characterId`,
@@ -21,6 +23,7 @@ const router = createBrowserRouter([
                 <CharacterPage />
             </Suspense>
         ),
+        errorElement: <ErrorBoundary />,
     },
 ]);
 
